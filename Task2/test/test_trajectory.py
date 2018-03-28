e1e5b4f9d9df8e158a1920769d3223da4979ab32
Mayank Bhargava <mayankmman@gmail.com>
2018-03-28 05:30:33 +0530

    error test

--- Task2/test/test_trajectory.py
+++ Task2/test/test_trajectory.py
@@ -1,18 +1,19 @@
+from __future__ import print_function
 import pandas as pd
 from sys import argv
 from math import sqrt
-traj1 = pd.read_csv('./log/'+argv[1]+'/trajectory_48.csv',headers=None);
-traj2 = pd.read_csv('./log/'+argv[2]+'/trajectory_48.csv',headers=None);
+traj1 = pd.read_csv('./log/'+argv[1]+'/trajectory_48.csv',header=None);
+traj2 = pd.read_csv('./log/'+argv[2]+'/trajectory_48.csv',header=None);
 err = 0.0
 for i in range(len(traj1)):
 	diff=traj1.iloc[i]-traj2.iloc[i]
 	diff = diff*(10**14)
-	diff = diff.abs()
 	diff = diff**2
-	diff = diff.sum()
+	diff = diff.mean()
 	diff = sqrt(diff)
+	diff = diff*(10**-14)
 	err = err + diff
-
+err = err/(len(traj1))
 print('Total error = '+str(err))
 		
 
