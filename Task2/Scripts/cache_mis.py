b37a99d4f03a36d2ff2d3b5b129ee9c0fa215cf8
Mayank Bhargava <mayankmman@gmail.com>
2018-03-31 21:10:24 +0530

    low cache access

---
+++ Task2/Scripts/cache_mis.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+import matplotlib.pyplot as plt
+import pandas as pd
+from sys import argv
+
+perfs = argv[2:]
+plt.figure(figsize=(8,8))
+plt.xlabel('Iteration number')
+plt.ylabel('Average Hit/Ref ratio (window=100)')
+plt.title('L1 Data Cache Stats')
+
+for ver in perfs:
+	cols = ['row','l1d_read_acc','l1d_read_mis']
+	data = pd.read_csv('./log/'+ver+'/perfLog_48.csv',header=None,names=cols)
+	data = data.astype(float)
+	
+	rat = data[cols[2]]
+	plt.plot(rat.rolling(window=100).mean().values)
+
+
+if len(perfs)==1:
+	plt.savefig('./Graphs/l1dcache_mis_'+argv[2]+'.png')
+else:
+	plt.legend(perfs)
+	plt.savefig('./Graphs/l1dcache_mis_comp_'+argv[1]+'.png')
+
